d6ad7d8a8edfdb580685cc138d7f6445bdec422e
Michael Maksoudian <mmaksoudian@gmail.com>
2020-10-20 21:07:33 -0700

    fixed build errors

--- src/Movie.h
+++ src/Movie.h
@@ -11,42 +11,18 @@ struct Movie {
 	friend ostream& operator<<(ostream& os, const Movie& movie) {
 		os << movie.title << endl;
 		os << movie.year << endl;
-		os << movie.genre << endl;
+		os << movie.genres << endl;
 		os << movie.duration << endl;
-		os << movie.country << endl;
-		os << movie.language << endl;
-		os << movie.director << endl;
-		os << movie.writer << endl;
+		os << movie.countries << endl;
+		os << movie.languages << endl;
+		os << movie.directors << endl;
+		os << movie.writers << endl;
 		os << movie.description << endl;
 		os << movie.actors << endl;
 		os << movie.rating << endl;
 		return os;
 	}
 
-//public:
-//	void SetTitle(const string& title);
-//	void SetYear(unsigned int year);
-//	void SetGenre(const string& genre);
-//	void SetDuration(unsigned int duration);
-//	void SetCountry(const string& country);
-//	void SetLanguage(const string& language);
-//	void SetDirector(const string& director);
-//	void SetWriter(const string& writer);
-//	void SetDescription(const string& description);
-//	void SetActors(const string& actors);
-//	void SetRating(double rating);
-//
-//	const string& GetTitle() const;
-//	unsigned int GetYear() const;
-//	const string& GetGenre() const;
-//	unsigned int GetDuration() const;
-//	const string& GetCountry() const;
-//	const string& GetLanguage() const;
-//	const string& GetDirector() const;
-//	const string& GetWriter() const;
-//	const string& GetDescription() const;
-//	const string& GetActors() const;
-//	double GetRating() const;
 	string title;
 	int year = -1;
 	string genres;
--- src/MovieParser.cpp
+++ src/MovieParser.cpp
@@ -4,28 +4,42 @@ void MovieParser::ParseMovie(MovieList& list, Tokenizer& tokenizer) {
 	Movie movie;
 
 	tokenizer.NextToken(); // imdb_title_id
+
 	movie.title = tokenizer.NextToken();
+
 	tokenizer.NextToken(); // original_title
+
 	movie.year = stoi(tokenizer.NextToken());
+
 	tokenizer.NextToken(); // date
-	movie.genre = tokenizer.NextToken();
+
+	movie.genres = tokenizer.NextToken();
+
 	movie.duration = stoi(tokenizer.NextToken());
-	movie.country = tokenizer.NextToken();
-	movie.language = tokenizer.NextToken();
-	movie.director = tokenizer.NextToken();
-	movie.writer = tokenizer.NextToken();
+
+	movie.countries = tokenizer.NextToken();
+
+	movie.languages = tokenizer.NextToken();
+
+	movie.directors = tokenizer.NextToken();
+
+	movie.writers = tokenizer.NextToken();
+
 	tokenizer.NextToken(); // production company
+
 	movie.actors = tokenizer.NextToken();
+
 	movie.description = tokenizer.NextToken();
+
 	movie.rating = stod(tokenizer.NextToken());
 
 	// skip the remaining tokens:
-	// budget
-	// usa_gross_income
-	// worlwide_gross_income
-	// metascore
-	// reviews_from_users
-	// reviews_from_critics
+	//    budget
+	//    usa_gross_income
+	//    worlwide_gross_income
+	//    metascore
+	//    reviews_from_users
+	//    reviews_from_critics
 
 	list.Add(movie);
 }
